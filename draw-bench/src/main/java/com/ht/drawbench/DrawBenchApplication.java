57016fbcc6dfabe52a2ed29a2d99ab451240799d
chengsukai <chengsukai@gmail.com>
2023-05-31 22:33:03 +0800

    添加定时任务

---
+++ draw-bench/src/main/java/com/ht/drawbench/DrawBenchApplication.java
@@ -0,0 +1,14 @@
+package com.ht.drawbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengsukai
+ */
+@SpringBootApplication
+public class DrawBenchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DrawBenchApplication.class, args);
+    }
+}
---
+++ fiber-platform-schedule/src/main/java/com/ht/schedule/DeviceStatusJob.java
@@ -0,0 +1,28 @@
+package com.ht.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.Date;
+
+/**
+ * @author chengsukai
+ */
+@Slf4j
+public class DeviceStatusJob extends QuartzJobBean {
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        // get parameters
+        context.getJobDetail().getJobDataMap().forEach(
+                (k, v) -> log.info("param, key:{}, value:{}", k, v)
+        );
+        // your logics
+        log.info("Hello Job执行时间: " + new Date());
+
+    }
+
+
+
+}
