b4e455e9dd748c3bd2d3e40b64c9c0b3c4be8d59
chengsukai <chengsukai@gmail.com>
2023-06-01 16:23:46 +0800

    添加解析ini文件依赖

---
+++ common/src/main/java/com/ht/base/utils/Ini4jFileVo.java
@@ -0,0 +1,61 @@
+package com.ht.base.utils;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+class Ini4jFileVo {
+    private String Level2;
+    private String Defect;
+    private String Airline;
+    private String Tfzjxx;
+    private String Tfzjsx;
+    private String Lxzjxx;
+    private String Lxzjsx;
+    private String ntbh;
+    // wtbh=PHICHE,TEST2,TEST4
+    private String wtbh;
+    private String ksgl;
+    private String scgl;
+    private String Mjbh;
+    private String Velocity;
+    private String SStrain;
+    private String XStrain;
+    private String Level1;
+    private String SBBH;
+    private String Text80;
+    private String Text83;
+    private String Text84;
+    private String Text81;
+    private String Text82;
+    private String Text85;
+    private String Text86;
+    private String Text87;
+    private String Text88;
+    private String Text89;
+    private String Text810;
+    private String Text811;
+    private String Text812;
+    private String Text813;
+    private String Text814;
+    private String Text815;
+    private String Text816;
+    private String Text817;
+    private String Text818;
+    private String Text819;
+    private String Text820;
+    private String Text821;
+    private String Text822;
+    private String Text823;
+    private String Text824;
+    private String Text825;
+    private String Text826;
+    private String Text827;
+    private String Text828;
+    private String Text829;
+    private String Text830;
+    private String Text831;
+    private String Text836;
+    private String Text837;
+}
---
+++ common/src/main/java/com/ht/base/utils/Ini4jUtils.java
@@ -0,0 +1,56 @@
+package com.ht.base.utils;
+
+import org.ini4j.Ini;
+import org.ini4j.Profile;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 读取ini文件
+ *
+ * @author chengsukai
+ */
+public class Ini4jUtils {
+
+    /**
+     * 读取ini文件的内容
+     *
+     * @param iniFile     ini文件
+     * @param fileContent ini文件中的key对应文件中的section，value对应i你文件section下的一个或多个key值
+     * @return
+     * @throws IOException
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public static Ini4jFileVo readIniFile(File iniFile, Map<String, List<String>> fileContent) throws IOException, NoSuchFieldException, IllegalAccessException {
+        Ini4jFileVo fileVo = new Ini4jFileVo();
+        Ini ini = new Ini();
+        ini.load(iniFile);
+        Profile.Section section = null;
+        Field field = null;
+        for (String key : fileContent.keySet()) {
+            section = ini.get(key);
+            for (String value : fileContent.get(key)) {
+                field = fileVo.getClass().getDeclaredField(value);
+                field.setAccessible(true);
+                field.set(fileVo, section.get(value));
+            }
+        }
+        return fileVo;
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchFieldException, IllegalAccessException {
+        File file = new File("/Users/chengsukai/Desktop/MyApp.ini");
+        Map<String, List<String>> fileContent = new HashMap<>();
+        fileContent.put("config", Arrays.asList("Level2", "Defect", "Airline", "Tfzjxx", "Tfzjsx", "Lxzjxx", "Lxzjsx", "ntbh", "wtbh", "ksgl", "scgl", "Mjbh", "Velocity", "SStrain", "XStrain", "Level1", "SBBH"));
+        fileContent.put("DDESection", Arrays.asList("Text80", "Text83", "Text84", "Text81", "Text82", "Text85", "Text86", "Text87", "Text88", "Text89", "Text810", "Text811", "Text812", "Text813", "Text814", "Text815", "Text816", "Text817", "Text818", "Text819", "Text820", "Text821", "Text822", "Text823", "Text824", "Text825", "Text826", "Text827", "Text828", "Text829", "Text830", "Text831", "Text836", "Text837"));
+        Ini4jFileVo fileVo = Ini4jUtils.readIniFile(file, fileContent);
+        System.out.println(fileVo);
+    }
+}
