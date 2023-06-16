0868b49230bbf75fd806801290e83225bbaabb08
chengsukai <chengsukai@gmail.com>
2023-06-16 09:25:18 +0800

    添加大盘审核校验

--- screening/src/main/java/com/ht/screening/controller/fiber/FiberCutController.java
+++ screening/src/main/java/com/ht/screening/controller/fiber/FiberCutController.java
@@ -3,6 +3,7 @@ package com.ht.screening.controller.fiber;
 import com.ht.base.domain.AjaxResult;
 import com.ht.screening.request.DrawInfoRequest;
 import com.ht.screening.request.FiberCutRequest;
+import com.ht.screening.service.impl.CheckService;
 import com.ht.screening.service.impl.FiberDrawingDefectServiceImpl;
 import com.ht.screening.service.impl.ScLs1ServiceImpl;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,14 +23,25 @@ public class FiberCutController {
     private FiberDrawingDefectServiceImpl fiberDrawingDefectService;
     @Resource
     private ScLs1ServiceImpl scLs1Service;
+    @Resource
+    CheckService checkService;
+
 
     @PostMapping("/fiberCut")
     public AjaxResult fiberCut(@RequestBody FiberCutRequest request) {
+        Boolean ischecked = checkService.checkDpcl(request.getFiberDiskNum());
+        if(!ischecked){
+            return AjaxResult.error("大盘检测未审核, 不能够筛选");
+        }
         return fiberDrawingDefectService.fiberCutDetail(request.getFiberDiskNum());
     }
 
     @PostMapping("/drawInfo")
     public AjaxResult drawInfo(@RequestBody DrawInfoRequest request){
+        Boolean ischecked = checkService.checkDpcl(request.getFiberDiskNum());
+        if(!ischecked){
+            return AjaxResult.error("大盘检测未审核, 不能够筛选");
+        }
         return scLs1Service.getDrawBenchInfo(request.getFiberDiskNum());
     }
     
