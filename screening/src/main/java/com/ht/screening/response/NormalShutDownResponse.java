f21dd1dd08ef37a5684423a7b649221be802faee
chengsukai <chengsukai@gmail.com>
2023-06-16 09:10:02 +0800

    适配前端请求参数

--- screening/src/main/java/com/ht/screening/response/NormalShutDownResponse.java
+++ screening/src/main/java/com/ht/screening/response/NormalShutDownResponse.java
@@ -9,6 +9,10 @@ import java.io.Serializable;
  */
 @Data
 public class NormalShutDownResponse implements Serializable {
+    /**
+     * 小盘条码
+     */
+    String xptm;
     Boolean status;
     Boolean printFlag;
 }
