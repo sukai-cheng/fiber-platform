f4c056bb24220b9176c44ca5d22c278ae044f61a
chengsukai <chengsukai@gmail.com>
2023-06-19 14:53:40 +0800

    添加caffine缓存

--- screening/src/main/java/com/ht/screening/ScreeningApplication.java
+++ screening/src/main/java/com/ht/screening/ScreeningApplication.java
@@ -3,12 +3,14 @@ package com.ht.screening;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author chengsukai
  */
 @SpringBootApplication
 @MapperScan("com.ht.screening.mapper")
+@EnableCaching
 public class ScreeningApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScreeningApplication.class, args);
