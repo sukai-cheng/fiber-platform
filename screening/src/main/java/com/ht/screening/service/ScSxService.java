bae72eda2bc8f59304c5b66ecf0efb94da5f7644
chengsukai <chengsukai@gmail.com>
2023-07-11 15:21:07 +0800

    删除无用代码

--- screening/src/main/java/com/ht/screening/service/ScSxService.java
+++ screening/src/main/java/com/ht/screening/service/ScSxService.java
@@ -3,11 +3,8 @@ package com.ht.screening.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.ht.base.domain.AjaxResult;
-import com.ht.screening.dto.CalFilterLenDto;
 import com.ht.screening.entity.ScSx;
-import com.ht.screening.entity.ScSx2;
 import com.ht.screening.request.FilterInfoRequest;
-import com.ht.screening.request.MainPlateInfoRequest;
 import com.ht.screening.vo.FiberFilterSmallDiskVo;
 
 import java.io.IOException;
