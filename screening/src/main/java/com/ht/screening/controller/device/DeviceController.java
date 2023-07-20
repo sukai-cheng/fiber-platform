5ee1c0496b13c2b294878c7430964860f0bfb944
chengsukai <chengsukai@gmail.com>
2023-07-20 09:27:34 +0800

    封版 1.0 修改代码

--- screening/src/main/java/com/ht/screening/controller/device/DeviceController.java
+++ screening/src/main/java/com/ht/screening/controller/device/DeviceController.java
@@ -28,16 +28,7 @@ public class DeviceController {
     @PostMapping("/getDeviceStatus")
     public AjaxResult getDeviceStatus(@RequestBody DeviceStatusRequest request) {
         try {
-//            DeviceInfoVo deviceInfoVo = deviceInfoService.getDeviceInfo(request.getFiberDiskNum());
-            DeviceInfoVo deviceInfoVo = new DeviceInfoVo();
-            deviceInfoVo.setActualVelocity(1500d);
-            deviceInfoVo.setPayOffLength(285.63);
-            deviceInfoVo.setRetractLength(35.84);
-            deviceInfoVo.setResidualLen(14.12);
-            deviceInfoVo.setAssignRetractLength(48.9);
-            deviceInfoVo.setActualTension(9.0);
-            deviceInfoVo.setNormalRun(0);
-            deviceInfoVo.setExceptionStop(1);
+            DeviceInfoVo deviceInfoVo = deviceInfoService.getDeviceInfo(request.getFiberDiskNum());
             return AjaxResult.success(deviceInfoVo);
         } catch (Exception e) {
             return AjaxResult.error("设备连接异常");
